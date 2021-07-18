371732cb45f85fd14d8b13562a1299ff1090401a
Nowsher Nahid <nowshernahid@Nowshers-MacBook-Air.local>
2021-07-18 18:42:50 +0600

    2nd commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,13 +11,19 @@ use App\Http\Controllers\CategoryController;
 
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
+// DASHBOARD ROUTES
 Route::get('/', [IndexController::class, 'index']);
+
+// CATEGORY ROUTES
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/add-categories', [CategoryController::class , 'store']);
 Route::get('/edit-category/{id}', [CategoryController::class , 'edit']);
 Route::post('/update-category/{id}', [CategoryController::class , 'update']);
 Route::get('/delete-category/{id}', [CategoryController::class, 'destroy']);
+
+// AUTHOR ROUTES
+Route::get('/categories', [AuthorController::class, 'index']);
+Route::post('/add-authors', [AuthorController::class , 'store']);
+Route::get('/edit-author/{id}', [AuthorController::class , 'edit']);
+Route::post('/update-author/{id}', [AuthorController::class , 'update']);
+Route::get('/delete-author/{id}', [AuthorController::class, 'destroy']);
